a36717a736f5270caf0fe8d9156775eb5d2d8114
Tyler Bates <Fondlytyler@gmail.com>
2020-07-02 21:28:17 -0700

    really this time added functionality to deleting beer in control.

--- src/components/BeerControl.js
+++ src/components/BeerControl.js
@@ -16,4 +16,65 @@ class BeerControl extends React.Component {
       editing: false
     };
   }
+
+  handleChangingSelectedBeer = (id) => {
+    const selectedBeer = this.state.masterBeerList.filter(beer => beer.id === id)[0];
+    this.setState({ selectedBeer: selectedBeer });
+  }
+
+  handleAddingNewBeerToList = (newBeer) => {
+
+    const newMasterBeerList = this.state.masterBeerList.concat(newBeer);
+    this.setState({
+      masterBeerList: newMasterBeerList,
+      counter: 0
+    });
+  }
+
+  handleClick = () => {
+    if (this.state.selectedBeer != null) {
+      this.setState({
+        formVisibleOnPage: false,
+        selectedBeer: null,
+        editing: false
+      });
+    } else if (this.state.counter === 0) {
+      this.setState(prevState => ({
+        counter: prevState.counter + 1
+      }));
+    } else {
+      this.setState(prevState => ({
+        formVisibleOnPage: !prevState.formVisibleOnPage,
+        counter: 0
+      }));
+    }
+  }
+
+  handleEditingBeerInList = (beerToEdit) => {
+    const editedMasterBeerList = this.state.masterBeerList
+      .filter(beer => beer.id !== this.state.selectedBeer.id)
+      .concat(beerToEdit);
+    this.setState({
+      masterBeerList: editedMasterBeerList,
+      editing: false,
+      selectedBeer: null
+    });
+  }
+
+  handleEditClick = () => {
+    console.log("handleEditClick reached!");
+    this.setState({ editing: true });
+  }
+
+  handleDeletingBeer = (id) => {
+    const newMasterBeerList = this.state.masterBeerList.filter(beer => beer.id !== id);
+    this.setState({
+      masterBeerList: newMasterBeerList,
+      selectedBeer: null
+    });
+  }
+
+
 }
+
+export default BeerControl;
