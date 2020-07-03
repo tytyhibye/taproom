b3e024251e284a86c1a51c6b21cbff4b2e1ff989
Tyler Bates <Fondlytyler@gmail.com>
2020-07-02 21:51:39 -0700

    add navbar to header.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,49 @@
-import React from "react";
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
 
 function Header() {
+  const classes = useStyles();
+
   return (
-    <React.Fragment>
-    <h3>Ty Ty's</h3><h1>TAP ROOM</h1>
-    </React.Fragment>
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="black" aria-label="home">
+            <HomeIcon />
+          </IconButton>
+          <IconButton edge="start" className={classes.menuButton} color="black" aria-label="add">
+            <PlusIcon />
+          </IconButton>
+          <IconButton edge="start" className={classes.menuButton} color="black" aria-label="list">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            Ty Ty's TAPROOM
+          </Typography>
+          {/* <Button color="black">Login</Button> */}
+        </Toolbar>
+      </AppBar>
+    </div>
   );
 }
+
 
 export default Header;
