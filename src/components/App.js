cb163ade1f1c99827895fd1b272ef43e13b73bbf
Tyler Bates <Fondlytyler@gmail.com>
2020-07-02 20:17:16 -0700

    add edit form functionality.

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
+import Header from "./Header";
+import BeerControl from "./BeerControl";
+import List from "./BeerList";
+import { Grid } from "@material-ui/core";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <Grid item><Header/></Grid>
+      <Grid container direction="column">
+        <Grid item xs={0} sm={2}/>
+        <Grid item xs={12} sm={5}>
+          <BeerControl/>
+        </Grid>
+        <Grid item xs={12} sm={5}>
+          <List/>
+        </Grid>
+      </Grid>
+    </React.Fragment>
   );
 }
 
