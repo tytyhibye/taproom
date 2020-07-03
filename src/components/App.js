3cfb6b3704fbeb9438672af7b79c0b7759fe76fc
Tyler Bates <Fondlytyler@gmail.com>
2020-07-02 22:30:07 -0700

    debugging attempt to customize background image via app.js.

--- src/components/App.js
+++ src/components/App.js
@@ -3,21 +3,23 @@ import Header from "./Header";
 import BeerControl from "./BeerControl";
 import List from "./BeerList";
 import { Grid } from "@material-ui/core";
-import './App.css';
+import '../App.css';
 
 function App() {
   return (
     <React.Fragment>
-      <Grid item><Header/></Grid>
-      <Grid container direction="column">
-        <Grid item xs={0} sm={2}/>
-        <Grid item xs={12} sm={5}>
-          <BeerControl/>
+      <div class="bg-image">
+        <Grid item><Header/></Grid>
+        <Grid container direction="column">
+          <Grid item xs={0} sm={2}/>
+          <Grid item xs={12} sm={5}>
+            <BeerControl/>
+          </Grid>
+          <Grid item xs={12} sm={5}>
+            <List/>
+          </Grid>
         </Grid>
-        <Grid item xs={12} sm={5}>
-          <List/>
-        </Grid>
-      </Grid>
+      </div>
     </React.Fragment>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
+// import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 
@@ -27,10 +27,10 @@ function Header() {
       <AppBar position="static">
         <Toolbar>
           <IconButton edge="start" className={classes.menuButton} color="black" aria-label="home">
-            <HomeIcon />
+            <MenuIcon />
           </IconButton>
           <IconButton edge="start" className={classes.menuButton} color="black" aria-label="add">
-            <PlusIcon />
+            <MenuIcon />
           </IconButton>
           <IconButton edge="start" className={classes.menuButton} color="black" aria-label="list">
             <MenuIcon />
